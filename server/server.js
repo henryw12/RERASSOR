841d404615a34245d86700427298e0901f885c01
henryw12 <85902369+henryw12@users.noreply.github.com>
2025-09-26 19:42:25 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ wss.on('connection', (ws, req) => {
     const params = new URLSearchParams(req.url.slice(1));
     const name = params.get("name");
     const secret = params.get("secret");
-    const clientType = params.get("clientType") || 'rover'; // If no type, it's a rover
+    const clientType = params.get("clientType") || 'rover'; // If no type, assume it's a rover
 
     ws.clientName = name;
     ws.clientType = clientType;
